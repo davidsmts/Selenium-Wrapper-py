82e319275eac576ec73283e6fd03353ac9718ed3
David Schmotz <davidschmotz@gmx.de>
2018-05-31 22:23:44 +0200

    First commit

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# @Author: David Schmotz <David>
+# @Date:   2018-05-31T22:09:52+02:00
+# @Email:  davidschmotz@gmail.com
+# @Filename: __init__.py
+# @Last modified by:   David
+# @Last modified time: 2018-05-31T22:14:56+02:00
+
+print("initted")
